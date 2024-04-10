24feca4ef561a8a54c2799cb7c4b9d1ca0afa72f
Thea <theola51924@stud.noroff.no>
2024-04-10 17:44:53 +0200

    added a testfile to test that the secrets work

---
+++ testSecret.js
@@ -0,0 +1,10 @@
+require("dotenv/config");
+
+function test(expectedSecret) {
+  const secretMatches = process.env.EXAMPLE_SECRET == expectedSecret;
+  console.log(`The example secret is ${process.env.EXAMPLE_SECRET}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  return secretMatches;
+}
+
+test("This is a secret");
