32164d5786efd59e8fe74817e8acedabe5f5a906
Thea <theola51924@stud.noroff.no>
2024-04-14 12:10:15 +0200

    added aliases to the the cypress commands to ba able to get the response status codes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,16 @@
 Cypress.Commands.add("navigateToLoginForm", () => {
-  cy.get("#registerForm")
-    .should("be.visible")
-    .parent()
-    .find('[data-auth="login"]')
-    .click();
+  cy.get("#registerForm").parent().find('[data-auth="login"]').click();
   cy.wait(500);
 });
 
-Cypress.Commands.add("testLoginCredentials", (email, password) => {
+Cypress.Commands.add("loginWithCredentials", (email, password) => {
+  cy.intercept({
+    method: "POST",
+    url: "**/api/v1/social/auth/login",
+  }).as("loginWithCredentials");
   cy.get("#loginEmail").type(email);
   cy.get("#loginPassword").type(password);
   cy.get("#loginForm").submit();
-  cy.wait(1000);
 });
 
 Cypress.Commands.add("login", (url, email, password) => {
@@ -19,9 +18,10 @@ Cypress.Commands.add("login", (url, email, password) => {
   cy.wait(500);
   cy.get("#registerForm").should("be.visible");
   cy.navigateToLoginForm();
-  cy.get("#loginForm").should("be.visible");
-  cy.get("#loginEmail").type(email);
-  cy.get("#loginPassword").type(password);
-  cy.get("#loginForm").submit();
-  cy.wait(1000);
+  cy.loginWithCredentials(email, password);
+  cy.wait("@loginWithCredentials").its("response.statusCode").should("eq", 200);
+  cy.intercept({
+    method: "GET",
+    url: "**/api/v1/social/profiles/**?&_followers=true&_posts=true&_following=true",
+  }).as("login");
 });
